abd5d27dbe7735bdff91a006b5133ed802a3326d
Clefaz <clefaz12@gmail.com>
2017-01-20 11:12:21 +0100

    up

--- index.js
+++ index.js
@@ -24,9 +24,32 @@ if (mongoose.connection.readyState == 0){
 }
 
 ///////////////////////////////////Commandes
+FUNCTIONS.pd = function(msg) {
+    msg.reply('C\'EST TAMER LE PD !');
+};    //DONE
+
 FUNCTIONS.ping = function(msg) {
     msg.reply('pong ! #joke');
 };    //DONE
+
+FUNCTIONS.pong = function(msg) {
+    msg.reply('c\' est !ping, abruti !!');
+};    //DONE
+
+FUNCTIONS.introduction = function(msg){
+    msg.channel.sendMessage('bonjour a tous !, je suis ' + bot.user + ' \n' +
+        'je suis ici pour vous aider autant que possible, après tout, c\'est ma principale fonction !\n' +
+        'vous pouvez essayer de discuter avec moi mais pas sûr que je réponde :/ je vous avoue que je n\'en suis qu\'à ma version 1.0 :p\n' +
+        'mais vous pourrez quand meme accéder a quelques commandes:\n' +
+        '```' +
+        '!help = je vous enverrai un MP avec de l\'aide ! :)\n' +
+        '!ping = si je suis en ligne, je vous répondrai (ne sers qu\'a tester la connexion.)\n'+
+        '!dbcheck = vérifie l\'état de la base de données, si jamais vous pensez qu\'il y a un soucis\n' +
+        '```' +
+        'Mais la fonction principale sera sur le channel quote !\n' +
+        'en effet a présent, toutes les quotes que vous posterez seront **enregistrées** ! et on pourra a l\'avenir les consulter ! :)' +
+        'c\'est tout pour le moment, il y aura plus de fonctionnalités dans de futures updates !');
+}
 
 FUNCTIONS.help = function(msg) {
     msg.author.sendMessage('help sent');
@@ -53,24 +76,24 @@ FUNCTIONS.deletemessages = function(msg, args) {
 };  //TODO fixer le deletemessage !delete HH:MM-JJ/MM/AA
 
 function quote(msg) {
-    mongoose.model('Quotes', QuoteSchema);
-
     var quote = msg.content.split('\"');
     if (quote.length != 3) {
         msg.channel.sendMessage('```MARKDOWN\n\#Quote Invalide\n' +
             '-> " insert quote here " -Auteur ```');
         return;
     }
+    if  (mongoose.connection.readyState == 0) {
+        msg.channel.sendMessage('```MARKDOWN\n\#Erreur de sauvegarde (server offline)```');
+        return;
+    }
+
+    mongoose.model('Quotes', QuoteSchema);
 
     var tmp = new mongoose.models.Quotes();
-
     var auteur = quote[2].split('-');
     tmp.author = auteur[auteur.length - 1];
     tmp.submitted_by = msg.author.username;
     tmp.quote = quote[1];
-
-    if  (mongoose.connection.readyState == 0)
-        msg.channel.sendMessage('```MARKDOWN\n\#Erreur de sauvegarde (server offline)```');
 
     tmp.save(function (err) {
         if (err){
@@ -84,6 +107,7 @@ function quote(msg) {
                 'envoyé par = ' + tmp.submitted_by + '```');
         }
     });
+
     msg.channel.fetchMessages({limit: 20})
         .then(function(messages) {
             messages.forEach(function (message) {
@@ -91,7 +115,7 @@ function quote(msg) {
                     message.delete();
             });
         });
-};  //TODO ajouter la date et l'ID
+};  //TODO ajouter la date et l'ID + affiner fonction delete
 //
 
 ///////////////////////////////////Events
@@ -103,18 +127,24 @@ bot.on('message', function(msg) {
     var args = msg.content.split(' ');
     if(args[0].substr(0, 1) == '!') {
         switch(args[0].substr(1)) {
+            case 'pd':
+                return FUNCTIONS.pd(msg);
             case 'ping':
                 return FUNCTIONS.ping(msg);
+            case 'pong':
+                return FUNCTIONS.pong(msg);
             case 'help':
                 return FUNCTIONS.help(msg);
             case 'dbcheck' :
                 return FUNCTIONS.dbcheck(msg);
+            case 'introduction':
+                return FUNCTIONS.introduction(msg);
             //case 'invit' :
             //    return FUNCTIONS.invit(msg);
             //case 'delete' :
             //    return FUNCTIONS.deletemessages(msg,args);
             default:
-                return msg.reply('Commande Invalide');
+                return msg.reply('Commande Invalide -> !help');
         }
     }
     if(args[0].substr(0, 1) == '\"' && msg.channel.name === 'quote')
